54d5d569215316e9f6162e48660ccccb27ae2f6e
MokoneSA <simonlephotojr@gmail.com>
2023-02-14 15:39:12 +0000

    11. All instances by class name

--- console.py
+++ console.py
@@ -102,6 +102,23 @@ class HBNBCommand(cmd.Cmd):
         else:
             storage.save()
 
+    def do_all(self, arg):
+        """Usage: all or all <class> or <class>.all()
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects."""
+        argl = parse(arg)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in storage.all().values():
+                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(argl) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
+
+"""
     def do_all(self, line):
         """Prints all string representation of all
         instances based or not on the class name"""
@@ -120,6 +137,7 @@ class HBNBCommand(cmd.Cmd):
                     objlist.append(str(obj))
             print(objlist)
 
+"""
     def do_update(self, line):
         """Updates an instance based on the class name
         and id by adding or updating attribute"""
